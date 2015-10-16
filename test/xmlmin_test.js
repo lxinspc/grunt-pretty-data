68a778a4b4394754d1643aa6fb37c04346ac4ff9
Nathan Adams <nathan.adams@bluefinsolutions.com>
2015-10-16 11:48:27 +0100

    Intial commit, from clone of grunt-xmlmin

---
+++ test/xmlmin_test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var grunt = require('grunt');
+
+exports.xmlmin = {
+    compile: function (test) {
+        test.expect(1);
+
+        var actual = grunt.file.read('tmp/test.xml'),
+            expected = grunt.file.read('test/expected/test.xml');
+        test.equal(actual, expected, 'should minify XML');
+
+        test.done();
+    },
+    multiple: function (test) {
+        test.expect(1);
+
+        var actual = grunt.file.read('tmp/multiple.xml'),
+            expected = grunt.file.read('test/expected/test.xml');
+        test.equal(actual, expected, 'should minify multiple XML files');
+
+        test.done();
+    },
+    empty: function (test) {
+        test.expect(1);
+
+        test.ok(!grunt.file.exists('tmp/idontexist.xml'), 'Empty minified file should not exist');
+
+        test.done();
+    }
+};
